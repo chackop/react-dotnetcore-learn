323a8c71e8c9842434c7347b2811452cf54b5e26
Chacko <chackop@gmail.com>
2019-11-17 21:40:28 +0000

    updated with speakers axios

--- reactwebpack/ClientApp/Client.js
+++ reactwebpack/ClientApp/Client.js
@@ -1,14 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import FullPage from "./Components/common/FullPage";
 
+import ReactDOM from "react-dom";
+import React from "react";
 import { browserHistory } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import FullPage from "./Components/common/FullPage"
 
-ReactDOM.hydrate(
+ReactDOM.render(
     <Router history={browserHistory}>
-        <FullPage/>
+      <FullPage />
     </Router>,
     document.getElementById("app")
 );
