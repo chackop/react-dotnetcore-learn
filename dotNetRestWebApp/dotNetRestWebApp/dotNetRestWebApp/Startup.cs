a9584dedb50560264398b7786bf9ddebade76206
Chacko <chackop@gmail.com>
2019-11-18 00:48:04 +0000

    Added project using VS gui. TODO newtonsoft dependency

--- dotNetRestWebApp/dotNetRestWebApp/dotNetRestWebApp/Startup.cs
+++ dotNetRestWebApp/dotNetRestWebApp/dotNetRestWebApp/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace dotNetRestWebApp
 {
@@ -36,11 +37,8 @@ namespace dotNetRestWebApp
                     .AllowCredentials());
             });
 
-            services.AddMvc()
-                .AddJsonOptions(options =>
-                {
-                    options.SerializerSettings.Formatting = Formatting.Indented;
-                });
+            // services.AddControllers()
+            //     .AddNewtonsoftJson(options => options.SerializerSettings.Formatting = Formatting.Indented);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
