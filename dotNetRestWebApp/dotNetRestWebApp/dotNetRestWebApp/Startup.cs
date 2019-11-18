9f6d59f5428b6bd8d250f6209618a134e9519d99
Chacko <chackop@gmail.com>
2019-11-18 00:28:03 +0000

    added project using VS mac

--- dotNetRestWebApp/dotNetRestWebApp/dotNetRestWebApp/Startup.cs
+++ dotNetRestWebApp/dotNetRestWebApp/dotNetRestWebApp/Startup.cs
@@ -25,6 +25,8 @@ namespace dotNetRestWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
             services.AddCors(options =>
             {
                 options.AddPolicy("CorsPolicy",
@@ -39,8 +41,6 @@ namespace dotNetRestWebApp
                 {
                     options.SerializerSettings.Formatting = Formatting.Indented;
                 });
-
-            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
